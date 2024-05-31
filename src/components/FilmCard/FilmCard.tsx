ac08901df293e44c1828817ff2b5e399d2186ed4
Gabriel Rodrigues <gabers357@gmail.com>
2024-05-31 14:03:21 -0300

    refactor: Update FilmCard component to support different view modes
    The code changes in this commit update the FilmCard component to support different view modes. The viewMode prop is added to the FilmCardProps interface and used to conditionally render the component based on the selected view mode. This enhancement allows for more flexibility in displaying film data, whether in a list or card format.

--- src/components/FilmCard/FilmCard.tsx
+++ src/components/FilmCard/FilmCard.tsx
@@ -7,39 +7,11 @@ import {
 
 interface FilmCardProps {
   filmData: Film;
+  viewMode: "list" | "card";
 }
-const FilmCard = ({ filmData }: FilmCardProps) => {
-  const test = true;
-  if (test) {
-    return (
-      <div className="card lg:card-side bg-base-100 shadow-xl">
-        {filmData.poster_path && (
-          <figure>
-            <img
-              src={`https://image.tmdb.org/t/p/w94_and_h141_bestv2/${filmData.poster_path}`}
-              alt={filmData.poster_path}
-            />
-          </figure>
-        )}
-        <div className="card-body">
-          <h2 className="card-title">
-            {filmData.title} <span>{filmData.original_title}</span>
-          </h2>
-          <div>
-            <h2>
-              {filmData.title} <span>{filmData.original_title}</span>
-            </h2>
-            <span>{filmData.release_date}</span>
-          </div>
-          {/* {filmData.overview && <p>{filmData.overview}</p>} */}
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Watch</button>
-          </div>
-        </div>
-      </div>
-    );
-  }
-  return (
+
+const FilmCard = ({ filmData, viewMode }: FilmCardProps) => {
+  return viewMode === "list" ? (
     <StyledFilmCardContainer>
       {filmData.poster_path && (
         <img
@@ -59,7 +31,28 @@ const FilmCard = ({ filmData }: FilmCardProps) => {
         </StyledFilmOverview>
       </StyledFilmDescriptionContainer>
     </StyledFilmCardContainer>
+  ) : (
+    <div className="card card-side bg-base-100">
+      {filmData.poster_path && (
+        <figure>
+          <img
+            src={`https://image.tmdb.org/t/p/w200/${filmData.poster_path}`}
+            alt={filmData.poster_path}
+          />
+        </figure>
+      )}
+      <div className="card-body">
+        <div className="flex flex-col">
+          <h3 className="card-title flex flex-col">
+            {filmData.title} <span>{filmData.original_title}</span>
+          </h3>
+          <span>{filmData.release_date}</span>
+        </div>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Watch</button>
+        </div>
+      </div>
+    </div>
   );
 };
-
 export default FilmCard;

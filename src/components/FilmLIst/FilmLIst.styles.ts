11d9a0692e66a7d554c1145a1f6b6dec0d0348b0
Gabriel Rodrigues <gabers357@gmail.com>
2024-06-01 00:23:46 -0300

    refactor: Update FilmCard and Navbar components
    This commit updates the FilmCard and Navbar components to improve the user interface and enhance the navigation experience within the application. The FilmCard component's button text is now in Portuguese, providing a better user experience for Portuguese-speaking users. The Navbar component now includes "Entrar" and "Sair" buttons for login and logout, respectively. Additionally, the "Mudar visualização" button is moved to the right side of the Navbar. These changes make the application more accessible and user-friendly.

--- src/components/FilmLIst/FilmLIst.styles.ts
+++ src/components/FilmLIst/FilmLIst.styles.ts
@@ -3,4 +3,6 @@ import styled from "styled-components";
 export const StyledFilmLIstContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
+  width: 100%;
+  justify-content: center;
 `;
---
+++ src/utils.ts
@@ -0,0 +1,5 @@
+export const token = import.meta.env.VITE_API_KEY;
+
+export const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+
+export const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;

78e8d1dd709c99be5e50c955ad160f023c9f874b
Gabriel Rodrigues <gabers357@gmail.com>
2024-06-01 01:38:26 -0300

    refactor: Update Navbar component to include "Assistidos" and "Favoritos" links
    This commit updates the Navbar component to include "Assistidos" and "Favoritos" links using React Router. The existing buttons are replaced with Link components that navigate to the "/watched/movies" and "/favorites/movies" routes respectively. This enhancement improves the navigation experience by providing clickable links instead of buttons, allowing users to easily switch between the watched and favorites pages.

--- src/main.tsx
+++ src/main.tsx
@@ -14,11 +14,11 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   {
-    path: "/favorites",
+    path: "/favorites/movies",
     element: <FavoriteMovies />,
   },
   {
-    path: "/watched",
+    path: "/watched/movies",
     element: <WatchedMovies />,
   },
 ]);

4da2ef47ea71a755855fe6ab46084721835b3dd8
Gabriel Rodrigues <gabers357@gmail.com>
2024-06-01 00:58:28 -0300

    refactor: Update Navbar component to include responsive layout and dropdown menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,12 +14,31 @@ const Navbar = ({
   const { isAuthenticated, user } = useAuth0();
   return (
     <div className="navbar bg-base-100 fixed top-0 w-full z-10 justify-between px-4">
-      <div className="flex-1 lg:flex-none">
+      <div className="flex-1">
         <p className="text-lg lg:text-xl">Ghibli Universe</p>
       </div>
-      <div className="flex-none lg:flex lg:items-center">
-        <div className="hidden lg:block">
-          <ul className="menu menu-horizontal px-1">
+      <div className="flex-none lg:hidden">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
             <li>
               <button className="btn btn-ghost">Assistidos</button>
             </li>
@@ -33,7 +52,23 @@ const Navbar = ({
             </li>
           </ul>
         </div>
-
+      </div>
+      <div className="flex-none hidden lg:flex lg:items-center">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <button className="btn btn-ghost">Assistidos</button>
+          </li>
+          <li>
+            <button className="btn btn-ghost">Favoritos</button>
+          </li>
+          <li>
+            <button onClick={onChangeViewClick} className="btn btn-ghost">
+              Mudar visualização
+            </button>
+          </li>
+        </ul>
+      </div>
+      <div className="flex-none gap-2 navbar-end">
         {isAuthenticated ? (
           <div className="dropdown dropdown-end">
             <div

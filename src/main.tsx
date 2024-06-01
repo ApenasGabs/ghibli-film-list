1f55424cee564ab8cab660e596e09bc0ef7a40ca
Gabriel Rodrigues <gabers357@gmail.com>
2024-06-01 01:33:15 -0300

    feat: Add React Router for navigation
    This commit adds React Router to the project to enable client-side routing. The "react-router-dom" npm package is included as a new dependency in the package.json file. The main.tsx file is updated to import and use the necessary components from React Router, such as createBrowserRouter and RouterProvider. Additionally, the Home and WatchedMovies pages are created as new files in the "src/pages" directory. This feature enhancement improves the navigation experience within the application, allowing users to easily switch between different pages and URLs.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,27 @@
 import { Auth0Provider } from "@auth0/auth0-react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import FavoriteMovies from "./pages/FavoriteMovies/FavoriteMovies.tsx";
+import Home from "./pages/Home/Home.tsx";
+import WatchedMovies from "./pages/WatchedMovies/WatchedMovies.tsx";
 import { clientId, domain } from "./utils.ts";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/favorites",
+    element: <FavoriteMovies />,
+  },
+  {
+    path: "/watched",
+    element: <WatchedMovies />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -12,7 +30,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       domain={domain}
       clientId={clientId}
     >
-      <App />
+      <RouterProvider router={router} />
     </Auth0Provider>
   </React.StrictMode>
 );
